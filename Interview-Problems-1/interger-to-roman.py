4191047662d4b574ec9d0f922a756bf578a40d63
moinatik <moinghori.it@gmail.com>
2023-11-04 16:23:42 -0500

    Integer to Roman Problem

---
+++ Interview-Problems-1/interger-to-roman.py
@@ -0,0 +1,21 @@
+def intTOroman(num):
+    val = [
+        1000, 900, 500, 400,
+        100, 90, 50, 40,
+        10, 9, 5, 4, 1
+    ]
+    symbols = [
+        "M", "CM", "D", "CD",
+        "C", "XC", "L", "XL",
+        "X", "IX", "V", "IV", "I"
+    ]
+
+    i =0
+    romanNum = ""
+    while num > 0:
+        div, num  = divmod(num, val[i])
+        romanNum += symbols[i] * div
+        i += 1
+    return romanNum
+num = 1999
+print(intTOroman(num))
