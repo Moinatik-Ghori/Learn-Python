281e9b8dbea723ecdd3a33d26a259c7a2b334562
moinatik <moinghori.it@gmail.com>
2023-11-05 21:37:44 -0600

    Roman to Integer Problem

--- Interview-Problems-1/interger-to-roman.py
+++ Interview-Problems-1/interger-to-roman.py
@@ -1,21 +1,47 @@
-def intTOroman(num):
-    val = [
-        1000, 900, 500, 400,
-        100, 90, 50, 40,
-        10, 9, 5, 4, 1
-    ]
-    symbols = [
-        "M", "CM", "D", "CD",
-        "C", "XC", "L", "XL",
-        "X", "IX", "V", "IV", "I"
-    ]
+def intTOroman(romanNum):
+    val = {
+        'M': 1000,
+        'CM': 900,
+        'D': 500,
+        'CD': 400,
+        'C': 100,
+        'XC': 90,
+        'L': 50,
+        'XL': 40,
+        'X': 10,
+        'IX': 9,
+        'V': 5,
+        'IV': 4,
+        'I' : 1
+    }
 
-    i =0
-    romanNum = ""
-    while num > 0:
-        div, num  = divmod(num, val[i])
-        romanNum += symbols[i] * div
-        i += 1
-    return romanNum
-num = 1999
-print(intTOroman(num))
+    l = len(romanNum)
+    i = 0
+    num = 0
+    char = ''
+    while i < l:
+
+        char = romanNum[i]
+        if romanNum[i] == 'C':
+            if (i+1) < l and romanNum[i+1] in ('M','D'):
+                char = char + romanNum[i+1]
+                print("Double Char",char)
+                i+=1
+        if romanNum[i] == 'X':
+            if (i+1) < l and romanNum[i+1] in ('C','L'):
+                char = char + romanNum[i+1]
+                print("Double Char",char)
+                i+=1
+        if romanNum[i] == 'I':
+            if (i+1) < l and romanNum[i+1] in ('X','V'):
+                char = char + romanNum[i+1]
+                print("Double Char",char)
+                i+=1
+        num = num + val[char]
+        i+=1
+        print("{} Char {} , Number {}".format(i,char,num))
+
+    return num
+
+romanNum = "MCMLXXXVII"
+print(intTOroman(romanNum))
