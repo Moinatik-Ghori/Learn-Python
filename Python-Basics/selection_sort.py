0cdc95dda911d7b7f61a35c91716bcfc046848bb
moinatik <moinghori.it@gmail.com>
2023-12-14 20:48:03 -0600

    Selection Sort

--- Python-Basics/selection_sort.py
+++ Python-Basics/selection_sort.py
@@ -1,7 +1,6 @@
 class Solution:
-    def selection_sort(self,lst):
+    def selection_sort_method_1(self,lst):
         print((lst))
-
         for i in range(len(lst)):
             midIndex = i
             for j in range(i+1,len(lst)):
@@ -10,6 +9,27 @@ class Solution:
             lst[midIndex],lst[i] = lst[i],lst[midIndex]
             print(f"List {lst}")
 
-list1 = [3,6,10,8,5,9,2,4,7,1]
+    def selection_sort_method_2(self,lst):
+        print(lst)
+        low = mid = 0
+        high = len(lst) -1
+        print(f"low={low}, mid={mid}, high={high}")
+        while mid <= high:
+            if lst[mid] == 0:
+                lst[low],lst[mid] = lst[mid],lst[low]
+                mid+=1
+                low+=1
+                print(f"when mid is 0, {lst}")
+            elif lst[mid] == 1:
+                mid+=1
+                print(f"when mid is 1, {lst}")
+            else:
+                lst[mid],lst[high] =lst[high],lst[mid]
+                print(f"when mid is 2, {lst}")
+                high-=1
+
+list1 = [2,0,2,1,1,0]
 l1 = Solution()
-l1.selection_sort(list1)
+l1.selection_sort_method_1(list1)
+list1 = [2,0,2,1,1,0]
+l1.selection_sort_method_2(list1)
